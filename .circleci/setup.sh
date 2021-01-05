e31acfc0fd2efea05a8ffd2fa0c7057df31fe539
Christian Brabandt <cb@256bit.org>
2021-01-05 12:42:52 +0100

    start app

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -3,6 +3,7 @@
 #set -x
 
 project="ism_ci"
+config="ismci"
 dir="/home/ibuser/iwaySDK/8.0.4/build2/projects/"
 
 # Setup some color variables
@@ -23,15 +24,19 @@ print_status () {
 
 (
   printf "\n${blue}Starting iSM Server Up\n"
-	pushd ~/iway8
+	pushd ~/iway8 >/dev/null
 	sed -i.bak -e '/^ *su/{s/^[^"]*"//;s/"$//}' bin/startservice.sh
-	bin/startservice.sh base >/dev/null
+	bin/startservice.sh base >/dev/null 2>&1
 	sleep 20
 )
 
 (
 	printf "${blue}Trying to access iSM Webconsole.....\t"
+	# TODO: why does the login fail?
+	set -xv
 	curl -L -s --user "${IWAY_USER}:${IWAY_PASS}" http://localhost:9999/ism >/dev/null
+	set +xv
+	
 	if [ $? -eq 7 ]; then
 		printf "${red}[FAILED]${reset}\n"
 	else
@@ -41,21 +46,21 @@ print_status () {
 
 
 (
-  printf "${blue}Copying %s project to iwaysdk build directory\n" "$project"
+  printf "${blue}Copying %s project to iwaysdk build directory\t" "$project"
 	ln -s ~/project "${dir}${project}"
 	print_status $?
 )
 
 (
+	pushd "${dir}/../configurations" >/dev/null
 	printf "${blue}Checking out iwaysdk Configuration.....\t"
-	pushd "${dir}/../configurations"
 	git clone --quiet --depth=1 https://github.com/chrisbra/ci_configuration ismci
 	print_status $?
 )
 
 (
+	pushd "${dir}/.." > /dev/null
 	printf "${blue}Patching iwaysdk build.sh .....\t"
-	pushd "${dir}/.."
 	sed -i.bak -e 's/exit 0/exit \$?/' build.sh
 	print_status $?
 	printf "${blue}make iwaysdk executable .....\t"
@@ -65,13 +70,16 @@ print_status () {
 
 
 (
+	pushd "${dir}/.." > /dev/null
 	printf "${blue}Building %s project for iSM .....\t" "$project"
-	pushd "${dir}/.."
-	sh build.sh BUILDAPP ismci
+	sh build.sh BUILDAPP "$config" > /dev/null
 	print_status $?
 	printf "${blue}Deploying %s project for iSM .....\t" "$project"
-	sh build.sh DEPLOYAPP ismci
+	sh build.sh DEPLOYAPP "$config" > /dev/null
+	print_status $?
+	printf "${blue}Starting %s app for iSM .....\t" "$project"
+	sh build.sh STARTAPP "$config"
 	print_status $?
 )
 
-printf "${green}DONE${reset}"
+printf "${green}DONE${reset}\n"
